92bcadd9bf51934b0237eabe1c3e1cd84857cd85
junior <trader.junior492047@gmail.com>
2021-02-23 13:43:42 +0700

    steam img 2

--- app.js
+++ app.js
@@ -17,16 +17,13 @@ const path = require('path');
 const fs = require('fs')
 const dir = './pic'
 io.on('connection', (socket) => {
-//   console.log(socket)
+    
 
-  socket.on('data', (msgg) => {
-    console.log(msgg)
-  })
-  // socket.emit('check-connection', 'connect success full')
   socket.on('RecieveBs64FromAI', (b64Img) => {
     console.log(b64Img)
     socket.emit('client-responeSteam', "data:image/jpeg;base64,"+ b64Img.toString("base64"));
   })
+  
     socket.on('MonitorSteamimg', (msg) => {
     if(msg == 'onSteam'){
         // const testFolder = './pic/';
