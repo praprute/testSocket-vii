8dcf65c873136bd520fd938a0d9d00910db7c78b
junior <trader.junior492047@gmail.com>
2021-02-23 13:21:26 +0700

    steam img 1

--- app.js
+++ app.js
@@ -17,38 +17,29 @@ const path = require('path');
 const fs = require('fs')
 const dir = './pic'
 io.on('connection', (socket) => {
-  console.log(socket)
-
-  socket.on('message-station', (idStation) => {
-    // console.log(idStation)
-    if(idStation){
-      socket.emit('client-connect')
-    }
-  })
+//   console.log(socket)
 
   socket.on('data', (msgg) => {
     console.log(msgg)
   })
   // socket.emit('check-connection', 'connect success full')
-    socket.on('message', (msg) => {
-    if(msg == 'test-mes'){
-        const testFolder = './pic/';
-        fs.readdir(testFolder, (err, files) => {
-            files.forEach(file => {
-            //console.log(`${file}`);
-            //socket.emit('client-connect', "data:image/png;base64,"+ file.toString("base64"));
-            setTimeout(function() {
-            fs.readFile(`./pic/${file}`, function(err, data){
-                // console.log(data)
-
-                    socket.emit('client-connect', "data:image/jpeg;base64,"+ data.toString("base64"));
-              
-                // socket.emit('imageConversionByClient', { image: true, buffer: data });
-                // socket.emit('client-connect', "data:image/png;base64,"+ data.toString("base64"));
-              });
-            });
-        }, 2000);
-          });
+    socket.on('MonitorSteamimg', (msg) => {
+    if(msg == 'onSteam'){
+        socket.on('RecieveBs64FromAI', (b64Img) => {
+            socket.emit('client-responeSteam', "data:image/jpeg;base64,"+ b64Img.toString("base64"));
+        })
+        // const testFolder = './pic/';
+        // fs.readdir(testFolder, (err, files) => {
+        //     files.forEach(file => {
+        //     setTimeout(function() {
+        //     fs.readFile(`./pic/${file}`, function(err, data){
+        //             socket.emit('client-connect', "data:image/jpeg;base64,"+ data.toString("base64"));
+        //         // socket.emit('imageConversionByClient', { image: true, buffer: data });
+        //         // socket.emit('client-connect', "data:image/png;base64,"+ data.toString("base64"));
+        //       });
+        //     });
+        // }, 2000);
+        //   });
     }
   });
 })
