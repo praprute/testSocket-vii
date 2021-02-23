fbd6ff2bdd37d9b35a63700b8fc664f705191287
junior <trader.junior492047@gmail.com>
2021-02-23 11:45:38 +0700

    1

---
+++ app.js
@@ -0,0 +1,58 @@
+
+const app = require('express')()
+const http = require('http').createServer(app)
+const cors = require('cors');
+app.use(cors({
+  origin: "*"
+}))
+const io = require('socket.io')(http, {
+  cors: {
+      origin: "*",
+      methods: "*",
+      allowedHeaders: "*",
+      credentials: false
+  }
+});
+const path = require('path');
+const fs = require('fs')
+const dir = './pic'
+io.on('connection', (socket) => {
+  console.log(socket)
+
+  socket.on('message-station', (idStation) => {
+    // console.log(idStation)
+    if(idStation){
+      socket.emit('client-connect')
+    }
+  })
+
+  socket.on('data', (msgg) => {
+    console.log(msgg)
+  })
+  // socket.emit('check-connection', 'connect success full')
+    socket.on('message', (msg) => {
+    if(msg == 'test-mes'){
+        const testFolder = './pic/';
+        fs.readdir(testFolder, (err, files) => {
+            files.forEach(file => {
+            //console.log(`${file}`);
+            //socket.emit('client-connect', "data:image/png;base64,"+ file.toString("base64"));
+            setTimeout(function() {
+            fs.readFile(`./pic/${file}`, function(err, data){
+                // console.log(data)
+
+                    socket.emit('client-connect', "data:image/jpeg;base64,"+ data.toString("base64"));
+              
+                // socket.emit('imageConversionByClient', { image: true, buffer: data });
+                // socket.emit('client-connect', "data:image/png;base64,"+ data.toString("base64"));
+              });
+            });
+        }, 2000);
+          });
+    }
+  });
+})
+
+http.listen(4001, function() {
+  console.log('listening on port 4001')
+})
