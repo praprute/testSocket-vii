cc1ce6331ce20236b55341495fa6d5b4da04f5f1
junior <trader.junior492047@gmail.com>
2021-03-22 12:43:52 +0700

    add api

--- controller/controller.js
+++ controller/controller.js
@@ -4,4 +4,10 @@ exports.test = (req, res, next) => {
     } = req;
 
     console.log(body)
+    const io = require('socket.io-client').connect('http://13.76.41.221:4001/', {
+            transports: ["websocket", "polling"],
+            withCredential: false
+        })
+
+    io.emit('clientResponeSteam', 'test API SUCCESS')
 }
