904fc42e9f15f6a6670f0b78372c18a3a53fabc5
junior <trader.junior492047@gmail.com>
2021-03-22 11:47:22 +0700

    fix app

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //const app = require('express')()
-const http = require('http').createServer(app)
-const cors = require('cors');
 const express      = require('express')
 const app          = express()
+const http = require('http').createServer(app)
+const cors = require('cors');
 const test = require('./routes/routeall')
 
 app.use(cors({
